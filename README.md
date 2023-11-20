d700dbd821268d2d2b5b49d76f2e182bab44daf9
kassio03 <email@address.comkassioferreira0324@gmail.com>
2023-11-19 23:24:09 -0300

    documentação e atualização do readme.md

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# monitoramento-temperatura-umidade
-Sistema de Monitoramento Ambiental de Temperatura e Umidade
+# Projeto de Monitoramento Ambiental com NodeMCU, DHT11 e MQTT
+
+## Descrição do Projeto:
+Este repositório contém o código-fonte e documentação do projeto de monitoramento ambiental utilizando NodeMCU, sensor DHT11 e MQTT. O sistema é projetado para medir a temperatura e umidade ambiente, enviando os dados para um servidor MQTT para posterior análise.
+
+## Funcionamento e Uso
+
+O sistema é iniciado quando o NodeMCU se conecta à rede Wi-Fi configurada. Em seguida, o sensor DHT11 realiza leituras periódicas de temperatura e umidade. Os dados são formatados em mensagens JSON e enviados para um servidor MQTT, permitindo o monitoramento remoto em tempo real.
+
+## Hardware Utilizado
+
+**Microcontrolador:** NodeMCU ESP8266.
+
+**Sensor:** DHT11 para medição de temperatura e umidade.
+
+**Atuador:** Buzzer Ativo 3V
+
+**Outros:** Protoboard, Jumpers
+
+## Como Reproduzir:
+
+Clone este repositório: 
+```bash
+    git clone https://github.com/seu-usuario/nome-do-repo.git
+```
+Siga as instruções de montagem no diretório /docs para conectar o NodeMCU e o sensor DHT11.
+
+Carregue o código-fonte do projeto para o NodeMCU.
+
+Acesse o servidor MQTT configurado e subscreva-se aos tópicos relevantes para receber os dados.
+
+
+## Licença
+
+Este projeto é distribuído sob a licença MIT. Consulte o arquivo [MIT license](LICENSE) para obter mais detalhes.
---
+++ src/Code_NodeMCU_DHT11/Code_NodeMCU_DHT11.ino
@@ -0,0 +1,105 @@
+#include <DHT.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+#define WIFI_SSID       "KNET_2G" // Nome da rede wifi
+#define WIFI_PASS       "" // Senha da rede wifi
+
+#define DHTPIN          2     // Pino onde o sensor DHT11 está conectado
+#define DHTTYPE         DHT11 // Tipo de sensor (DHT11, DHT22, AM2302)
+
+#define BUZZER_PIN      4 // Pino onde o buzzer ativo está conectado
+
+#define MQTT_SERVER     "test.mosquitto.org"  // Endereço do broker
+#define MQTT_PORT       1883  // porta
+#define MQTT_USER       ""  // O nome de usuário configurado no broker
+#define MQTT_PASSWORD   ""    // A senha configurada no broker
+#define MQTT_TOPIC      "ksdht11/th" // tópico para publicação
+
+#define WARNING_TEMP    40 // Em celsius
+#define WARNING_HUM     30 // Porcentagem mínima de UR
+
+DHT dht(DHTPIN, DHTTYPE);
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+struct SensorData {
+  float temperature;
+  float humidity;
+};
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Conectando ao WiFi...");
+  }
+  Serial.println("Conectado ao WiFi");
+
+  pinMode(BUZZER_PIN, OUTPUT);
+
+  client.setServer(MQTT_SERVER, MQTT_PORT);
+}
+
+void loop() {
+  //  Leitura do sensor
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
+
+  //  Print no serial dos dados obtidos
+  Serial.print("Temperatura: ");
+  Serial.print(temp);
+  Serial.println(" °C");
+  Serial.print("Umidade: ");
+  Serial.print(hum);
+  Serial.println("%");
+
+  // Toque de alerta em caso de temperatura acima do esperado
+  if (temp > WARNING_TEMP) {
+    tone(BUZZER_PIN, 500);
+    delay(400);
+    noTone(BUZZER_PIN);
+  }
+
+  // Toque de alerta em caso de umidade relativa do ar abaixo do esperado
+  if (hum < WARNING_HUM) {
+    tone(BUZZER_PIN, 800);
+    delay(150);
+    noTone(BUZZER_PIN);
+
+    delay(50);
+
+    tone(BUZZER_PIN, 800);
+    delay(150);
+    noTone(BUZZER_PIN);
+  }
+
+  client.connect("NodeMCU_Client", MQTT_USER, MQTT_PASSWORD);
+  
+  if (client.connected()) {
+    struct SensorData data;
+    data.temperature = temp;
+    data.humidity = hum;
+
+    // Convertendo a estrutura SensorData para uma string
+    char payload[50];  // ajuste o tamanho conforme necessário
+    sprintf(payload, "{\"temperature\": %.2f, \"humidity\": %.2f}", data.temperature, data.humidity);
+
+    Serial.println("Conectado ao Broker, tentando publicar...");
+
+    // Publicando a string formatada
+    if (client.publish(MQTT_TOPIC, payload)) {
+      Serial.println("Publicação de temperatura bem-sucedida");
+    } else {
+      Serial.println("Falha na publicação de temperatura");
+    }
+  } else {
+    Serial.println("Não conectado ao Broker");
+    return;
+  }
+  client.disconnect();
+  delay(2000); // Intervalo de leitura
+}
